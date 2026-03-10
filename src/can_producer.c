ad13793307c3419cc6ed63373ff877f26a639271
Gordon McLellan <gordonthree@gmail.com>
2026-03-10 01:24:54 -0400

    added lastProducerTick global

--- src/can_producer.c
+++ src/can_producer.c
@@ -1,1 +1,6 @@
 #include "can_producer.h"
+
+/* ===== GLOBALS ===== */
+
+/** Producer tick counter */
+uint32_t lastProducerTick[MAX_SUB_MODULES] = {0}; 
--- src/can_producer.h
+++ src/can_producer.h
@@ -2,3 +2,6 @@
 
 #include "canbus_project.h" /**< Required for subModule_t */
 #include "node_state.h"     /**< Required for node and trackers */
+
+/** Producer tick counter */
+extern uint32_t lastProducerTick[]; /**< Array of producer tick counters */
